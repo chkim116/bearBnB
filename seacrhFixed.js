06fe8fd6487e2ffb6268e069773278b3a6f300a0
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-23 23:20:01 +0900

    prittier

--- seacrhFixed.js
+++ seacrhFixed.js
@@ -7,23 +7,22 @@ const searchHeight = searchLocation.getBoundingClientRect().height;
 const asideHeight = aside.getBoundingClientRect().height;
 const headerHegihts = headerHegiht(asideHeight, searchHeight);
 
-function headerHegiht (a, b) {
-    return a + b
+function headerHegiht(a, b) {
+  return a + b;
 }
 
 function scrollLocation() {
-    if (window.scrollY < headerHegihts) {
-        searchLocation.classList.remove("search-fixed");
-    } else {
-        searchLocation.classList.add("search-fixed");
-    }
+  if (window.scrollY < headerHegihts) {
+    searchLocation.classList.remove("search-fixed");
+  } else {
+    searchLocation.classList.add("search-fixed");
+  }
 }
 
 document.addEventListener("scroll", scrollLocation);
 
 // 변해야 하는 조건값은 변수로 지정하지 말것.
 // 높이 값은 특히 그렇다.
-
 
 // delbtn X 추가
 const searchForm = document.querySelector(".search__form");
@@ -33,29 +32,25 @@ const span = document.createElement("span");
 span.innerText = "X";
 
 function delInput() {
-    searchInput.value = "";
+  searchInput.value = "";
 }
 
 function spanDel() {
-    
-    if (Number(view) > window.innerWidth) {
-        searchForm.appendChild(span);
-        span.style.display ="inline";
-    } else { 
-        span.style.display ="none";
-    }
+  if (Number(view) > window.innerWidth) {
+    searchForm.appendChild(span);
+    span.style.display = "inline";
+  } else {
+    span.style.display = "none";
+  }
 }
 
-window.addEventListener("resize", spanDel)
+window.addEventListener("resize", spanDel);
 
 // form에 제출하면 alert 알림창 보여주기
-function handleSubmit (event) {
-    event.preventDefault();
-    alert(searchInput.value);
-    searchInput.value = "";
+function handleSubmit(event) {
+  event.preventDefault();
+  alert(searchInput.value);
+  searchInput.value = "";
 }
 
 searchForm.addEventListener("submit", handleSubmit);
-
-
-
